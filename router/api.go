d7b0b5d191ab5f5e1d09272da464d40480126c88
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 00:39:08 +0100

    serve only index

--- router/api.go
+++ router/api.go
@@ -4,13 +4,5 @@ import "net/http"
 // import "io/ioutil"
 
 func ApiManual(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/api/" {
-		http.ServeFile(w,r,"html/api/index.html")
-	} else if r.URL.Path == "/api/style.css" {
-		http.ServeFile(w,r,"html/api/style.css")
-	} else if r.URL.Path == "/api/script.js" {
-		http.ServeFile(w,r,"html/api/script.js")
-	} else {
-		w.Write([]byte("400 bad request"))
-	}
+	http.ServeFile(w,r,"html/api/index.html")
 }
