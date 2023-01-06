f1f54d6fce4565521614746da7e5f3955750465d
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 00:12:07 +0100

    serve static files

--- router/api.go
+++ router/api.go
@@ -1,7 +1,16 @@
 package router
 
 import "net/http"
+// import "io/ioutil"
 
 func ApiManual(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r,"html/api.html")
+	if r.URL.Path == "/api/" {
+		http.ServeFile(w,r,"html/api/index.html")
+	} else if r.URL.Path == "/api/style.css" {
+		http.ServeFile(w,r,"html/api/style.css")
+	} else if r.URL.Path == "/api/script.js" {
+		http.ServeFile(w,r,"html/api/script.js")
+	} else {
+		w.Write([]byte("400 bad request"))
+	}
 }
