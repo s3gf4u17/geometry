41021ff9b15f0a351255fe8ee5a4e36fb7eb45fb
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 00:38:50 +0100

    temp move js and css into html

--- html/api/script.js
+++
@@ -1,8 +0,0 @@
-function expand(event){
-    var target = event.target.parentElement
-    if (window.getComputedStyle(target).maxHeight === "29px"){
-        target.style.maxHeight = "none";
-    } else {
-        target.style.maxHeight = "29px";
-    }
-}
