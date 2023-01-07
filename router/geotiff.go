8a9e5c9621b8dbf16fa0dbfaf469a3e193a5a358
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 12:11:33 +0100

    imports

--- router/geotiff.go
+++ router/geotiff.go
@@ -4,9 +4,13 @@ import "net/http"
 import "new-gis-portal/model"
 import "encoding/json"
 import "io/ioutil"
+import "strings"
+import "fmt"
+import "os"
+import "io"
 
 func isValid(source string) bool {
-	files,_:=ioutil.ReadDir("data/geojson")
+	files,_:=ioutil.ReadDir("data/geotiff")
 	for _,file := range files {
 		if !file.IsDir() && file.Name() == source {return true}
 	}
@@ -21,4 +25,24 @@ func GeotiffLayers(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(layers)
+}
+
+func GeotiffLayers(w http.ResponseWriter, r *http.Request) {
+	source := strings.TrimPrefix(r.URL.Path, "/api/geotiff/")
+	method := r.Method
+	if source == "" && method == "GET" { // get all available layers
+		layers:=[]model.GeoTiffLayer{}
+		files,_:=ioutil.ReadDir("data/geotiff")
+		for _,file := range files {if !file.IsDir() {layers=append(layers,model.GeoTiffLayer{file.Name()})}}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(layers)
+	} else if source == "" && method == "POST" { // create new geojson layer
+		file,header,_:=r.FormFile("file")
+		fmt.Printf("%s",header.Filename)
+		ondisk,_:=os.Create("data/geotiff/"+header.Filename)
+		io.Copy(ondisk,file)
+	} else if source != "" && method == "GET" { // read geojson layer
+		var valid bool = isValid(source)
+		if valid {http.ServeFile(w,r,"data/geotiff/"+source)}
+	}
 }
