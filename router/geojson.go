5ee61a011972a1238d9ed8b22fcd24d12fc1dbfc
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 12:05:23 +0100

    save uploaded bytes into ondisk created file

--- router/geojson.go
+++ router/geojson.go
@@ -5,6 +5,9 @@ import "new-gis-portal/model"
 import "encoding/json"
 import "io/ioutil"
 import "strings"
+import "fmt"
+import "os"
+import "io"
 
 func isValid(source string) bool {
 	files,_:=ioutil.ReadDir("data/geojson")
@@ -24,7 +27,10 @@ func GeojsonLayers(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(layers)
 	} else if source == "" && method == "POST" { // create new geojson layer
-
+		file,header,_:=r.FormFile("file")
+		fmt.Printf("%s",header.Filename)
+		ondisk,_:=os.Create("data/geojson/"+header.Filename)
+		io.Copy(ondisk,file)
 	} else if source != "" && method == "GET" { // read geojson layer
 		var valid bool = isValid(source)
 		if valid {http.ServeFile(w,r,"data/geojson/"+source)}
