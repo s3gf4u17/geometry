4e65a283821186a79d42c17f275b2855c2fa79f3
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 11:41:34 +0100

    end with backslash (to extract parameters)

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "net/http"
 import "new-gis-portal/router"
 
 func main() {
-	http.HandleFunc("/api/geojson",router.GeojsonLayers)
-	http.HandleFunc("/api/geotiff",router.GeotiffLayers)
+	http.HandleFunc("/api/geojson/",router.GeojsonLayers)
+	http.HandleFunc("/api/geotiff/",router.GeotiffLayers)
 	http.HandleFunc("/api/",router.ApiManual)
 	http.ListenAndServe("localhost:8000",nil)
 }
