84ef8c02537b78d8a5d95d43a137fbf2823ca08d
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 11:42:00 +0100

    security url check

--- router/api.go
+++ router/api.go
@@ -1,8 +1,13 @@
 package router
 
 import "net/http"
-// import "io/ioutil"
+import "fmt"
 
 func ApiManual(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	if r.URL.Path != "/api/" {
+		w.Write([]byte("400 bad request"))
+		return
+	}
 	http.ServeFile(w,r,"html/api/index.html")
 }
