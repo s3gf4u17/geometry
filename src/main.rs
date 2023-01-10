1835926847ae82447cbf176b691ae6cfe2cace45
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-10 12:37:24 +0100

    match parsed guess

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,11 @@ fn main() {
 
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Not a number!");
+        // variable shadowing
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num)=>num,
+            Err(_)=>continue,
+        };
 
         // println!("You guessed {}",guess);
 
