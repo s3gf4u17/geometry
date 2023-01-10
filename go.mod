9b72f404af02c5b3ed52b99015da4542b2f87721
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-10 11:37:34 +0100

    remove old code base

--- go.mod
+++
@@ -1,3 +0,0 @@
-module new-gis-portal
-
-go 1.18
--- model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type GeoJsonLayer struct {
-	Source string `json:"source"`
-}
-
-type GeoTiffLayer struct {
-	Source string `json:"source"`
-}
