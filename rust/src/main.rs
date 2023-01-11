357a2f0f2a885d04b930564d263a6ad70a0c9c7f
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-11 18:18:12 +0100

    ownership

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,7 +14,42 @@ fn main () {
     let b = a; // copy
     println!("{} {}",a,b);
     let s1 = String::from("string"); // heap variable 
-    let s2 = s1; // transfer ownership
+    let s2 = s1; // move ownership
+    let s3 = s2.clone(); // creates a clone instead of moving ownership
     // ERROR println!("{} {}",s1,s2); - ownership transfered from s1 to s2
-    println!("{}",s2);
+    println!("{} {}",s2,s3);
+    takes_ownership(s3);
+    // println!("{} {}",s2,s3); - s3 ownership moved
+    makes_copy(b);
+    println!("{}",b); // b still can be used (its stored on a stack)
+    let s2=takes_and_gives_back(s2);
+    println!("{}",s2); // s2 can still be used (ownership returned)
+
+    let (len,s2) = calculate_length_ownership(s2);
+    println!("{} {} {} {}",s2,len,calculate_length_reference(&s2),s2);
 }
+
+fn takes_ownership(s: String) {
+    println!("{}",s);
+}
+
+fn makes_copy(i: i32) {
+    println!("{}",i);
+}
+
+fn takes_and_gives_back(s: String) -> String {
+    println!("{}",s);
+    s
+}
+
+fn calculate_length_ownership(s:String) -> (usize,String) {
+    (s.len(),s)
+}
+
+fn calculate_length_reference(s:&String) -> usize { // references do not take ownership (borrowing) - references are immutable by default, you can set it to mutable, but you can borrow mutable value only once in a scope
+    s.len()
+}
+
+// fn returns_reference() -> &'static String {
+//     String::from("hello") // string is only in scope of this function, so rust will drop it when function finishes (reference pointing to invalid memory) we have to use lifetime
+// }
